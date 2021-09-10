b25527f056d5ee3e3d8ed968293bf8db5cf23add
WallacX <88462062+WallacX@users.noreply.github.com>
2021-09-10 13:50:29 -0300

    Update ui_clientes.py

--- ui/ui_clientes.py
+++ ui/ui_clientes.py
@@ -4,27 +4,30 @@ from PyQt5 import uic
 
 class CadClientes(QWidget):
     def __init__(self):
-        super().__init__()
-        # carrega os componentes (Widgets) para dentro da classe
-        uic.loadUi("ui/ui_clientes.ui", self)
-
-        # definindo os eventos dos botões
+        super(). __init__()
+        uic.loadUi("ui/ui_clientes.ui",self)
         self.b_salvar.clicked.connect(self.salvarCliente)
         self.b_excluir.clicked.connect(self.excluirCliente)
 
     def salvarCliente(self):
         nome = self.nome.text()
-        rg = self.rg.text()
         cpf = self.cpf.text()
-    
+        telefone = self.telefone.text()
+        email = self.email.text()
+        endereco = self.endereco.text()
+
         print("Salvar cliente...")
         self.informe.setText("Cliente salvo com sucesso!")
         self.limparCampos()
 
     def excluirCliente(self):
         print("Excluir cliente")
+        self.informe.setText("Cliente excluido com sucesso!")
+        self.limparCampos()
 
     def limparCampos(self):
         self.nome.setText("")
-        self.rg.setText("")
         self.cpf.setText("")
+        self.telefone.setText("")
+        self.email.setText("")
+        self.endereco.setText("")
