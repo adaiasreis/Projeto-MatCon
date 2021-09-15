12a60f64dd10ae6809cf08c74bd9e666f0c407c2
rafatosta <rafa.ecomp@gmail.com>
2021-09-15 10:52:48 -0300

    combox cliente

--- layouts/ui_vendas.py
+++ layouts/ui_vendas.py
@@ -9,15 +9,33 @@ class NovaVenda(QWidget):
         super(). __init__()
         uic.loadUi("ui/ui_novavenda.ui", self)
 
+        self.clienteAtual = None
+        self.lista_clientes = []
+
+        self.setEventos()
+
         self.carregaDadosClientes()
         #self.carregaDadosProdutos()
+    
 
     def carregaDadosClientes(self):
         # dados do cliente
-        lista = ClientesModel.getClientes()
+        self.lista_clientes = ClientesModel.getClientes()
         lista_combo = []
-        for c in lista:
-            lista_combo.append(str(c.id) + " - "+c.nome)
+        for c in self.lista_clientes:
+            lista_combo.append(c.nome)
         self.combo_clientes.addItems(lista_combo)
+    
+    def setEventos(self):
+        # Sends the current index (position) of the selected item.
+        self.combo_clientes.currentIndexChanged.connect( self.index_changed )
+        
+    
+    def index_changed(self, i): # i is an int
+        print(self.lista_clientes[i].nome)
+        self.clienteAtual = self.lista_clientes[i]
+        self.id_lineEdit.setText(str(self.lista_clientes[i].id))
+
+
 
 
