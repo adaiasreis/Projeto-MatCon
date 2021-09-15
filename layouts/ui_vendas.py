4da216aea584809dc0170f5c38f0ea4f29efb3d0
rafatosta <rafa.ecomp@gmail.com>
2021-09-15 15:18:29 -0300

    ui de vendas

--- layouts/ui_vendas.py
+++ layouts/ui_vendas.py
@@ -12,11 +12,12 @@ class NovaVenda(QWidget):
 
         self.clienteAtual = None
         self.lista_clientes = []
+        self.lista_produtos = []
 
         self.setEventos()
 
         self.carregaDadosClientes()
-        # self.carregaDadosProdutos()
+        self.carregaDadosProdutos()
 
     def carregaDadosClientes(self):
         # dados do cliente
@@ -25,13 +26,25 @@ class NovaVenda(QWidget):
         for c in self.lista_clientes:
             lista_combo.append(c.nome)
         self.combo_clientes.addItems(lista_combo)
+    
+    def carregaDadosProdutos(self):
+        # dados do cliente
+        self.lista_produtos = ProdutosModel.getProdutos()
+        lista_combo = []
+        for c in self.lista_produtos:
+            lista_combo.append(c.nome)
+        self.combo_produtos.addItems(lista_combo)
 
     def setEventos(self):
         # Envia a posição atual do item
-        self.combo_clientes.currentIndexChanged.connect(self.index_changed)
+        self.combo_clientes.currentIndexChanged.connect(self.index_changed_cliente)
+        self.combo_produtos.currentIndexChanged.connect(self.index_changed_produto)
 
-    def index_changed(self, i):  # i é a posição do item selecionado
+    def index_changed_cliente(self, i):  # i é a posição do item selecionado
         print(self.lista_clientes[i].nome)
         # a lista do comboBox e a lista de clientes possuem o mesmo tamanho e itens, logo são iguais e podemos pegar o mesmo item da lista, o objeto cliente desejado
         self.clienteAtual = self.lista_clientes[i]
         self.id_lineEdit.setText(str(self.lista_clientes[i].id))
+    
+    def index_changed_produto(self, i):  # i é a posição do item selecionado
+        print(self.lista_produtos[i].nome)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow 
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
 
 from layouts.ui_produtos import CadProdutos
@@ -9,37 +9,41 @@ from layouts.ui_vendas import NovaVenda
 from qt_material import apply_stylesheet
 
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ui/mainwindow.ui", self)
 
-        # define os eventos dos botões
+        # define os itens da lista
         self.listWidget.insertItem(0, "PRODUTOS")
         self.listWidget.insertItem(1, "CLIENTES")
         self.listWidget.insertItem(2, "NOVA VENDA")
         #self.listWidget.insertItem(3, "LISTA DE VENDAS")
 
+        # seleciona o primeiro item como padrão
+        self.listWidget.setCurrentRow(0)
+
+        # carrega todas as janelas de uma só vez
         self.carregaJanelas()
-        
-        #evento para selecionar a página
+
+        # evento para selecionar a página
         self.listWidget.currentRowChanged.connect(self.display)
 
     def carregaJanelas(self):
-        self.stackedWidget.insertWidget(0,CadProdutos())
-        self.stackedWidget.insertWidget(1,CadClientes())
-        self.stackedWidget.insertWidget(2,NovaVenda())
+        """obs.: é possível carregar as janelas por demanda. 
+            Ou seja, verifica qual o index do listWidget selecionado e insere a janela na respectiva posição """
+        self.stackedWidget.insertWidget(0, CadProdutos())
+        self.stackedWidget.insertWidget(1, CadClientes())
+        self.stackedWidget.insertWidget(2, NovaVenda())
 
     def display(self, index):
-        #necessário carregar as janelas a cada trasição para atualizar as informações
+        # necessário carregar as janelas a cada trasição para atualizar as informações
         self.carregaJanelas()
         self.stackedWidget.setCurrentIndex(index)
-        
 
 
 app = QApplication(sys.argv)
-#aplicar o tema no programa
+# aplicar o tema no programa
 apply_stylesheet(app, theme='dark_blue.xml')
 
 window = MainWindow()
