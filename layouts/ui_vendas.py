c56ce5b8b35fe9df388a8dd791d30c9581108bcc
rafatosta <rafa.ecomp@gmail.com>
2021-09-15 20:29:11 -0300

    validação do botão Adicionar item

--- layouts/ui_vendas.py
+++ layouts/ui_vendas.py
@@ -30,6 +30,7 @@ class NovaVenda(QWidget):
 
         # para valição do campo QUANTIDADE
         # só é permitido valores inteiros e número com 5 algarismos
+        """"mais exemplos: https://www.programcreek.com/python/example/106688/PyQt5.QtGui.QRegExpValidator"""
         qtd_validator = QRegExpValidator(QRegExp('^[0-9]{5}$'), self.qtd)
         self.qtd.setValidator(qtd_validator)
 
@@ -65,6 +66,9 @@ class NovaVenda(QWidget):
         # remove o item selecionado
         self.btn_remover_item.clicked.connect(self.limparSelecionado)
 
+        # verifica a quantidade digitada
+        self.qtd.textEdited.connect(self.qtd_edited)
+
     # CLIENTE
     def index_changed_cliente(self, i):  # i é a posição do item selecionado
         # a lista do comboBox e a lista de clientes possuem o mesmo tamanho e itens, logo são iguais e podemos pegar o mesmo item da lista, o objeto cliente desejado
@@ -84,6 +88,7 @@ class NovaVenda(QWidget):
         item = ItemVenda(self.qtd.text(), self.produtoAtual)
         self.tabelaItens._addRow(item)
         self.btn_limpar_itens.setEnabled(True)
+        self.btn_add_item.setEnabled(False)
 
     # adiciona um item na tabela
     def limparItens(self):
@@ -92,3 +97,11 @@ class NovaVenda(QWidget):
     # adiciona um item na tabela
     def limparSelecionado(self):
         self.tabelaItens.limparSelecionado()
+
+    # executa a função toda vez que for digitado no campo QUANTIDADE
+    def qtd_edited(self, s):
+        # habilita o botão de adicionar apenas se estiver a quantidade disponível
+        if int(s) <= self.produtoAtual.quantidade:
+            self.btn_add_item.setEnabled(True)
+        else:
+            self.btn_add_item.setEnabled(False)
