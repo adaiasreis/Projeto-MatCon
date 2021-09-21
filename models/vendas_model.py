3ee422f0ef58d54c7f69920d59d5784aed72ce9e
rafatosta <rafa.ecomp@gmail.com>
2021-09-21 09:57:04 -0300

    fix

--- models/vendas_model.py
+++ models/vendas_model.py
@@ -4,7 +4,9 @@ import models.database as db
 def addVenda(venda):
     sql_addVenda = "INSERT INTO Vendas (id_cliente, valor_total) VALUES (?,?)"
 
-    sql_id_venda= ""
+    # só funciona logo após o insert
+    sql_id_venda= "SELECT LAST_INSERT_ROWID() AS id;"
 
     for item in venda.lista_de_itens:
         #add os itens na tabela de itens
+        pass
